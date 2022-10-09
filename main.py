cf5474daf41764cc646b86fe007eb3d722e01e97
becca <93043262+beccaric@users.noreply.github.com>
2022-10-09 12:48:56 +0000

    HLT 2 - Going Loopy

--- main.py
+++ main.py
@@ -8,4 +8,11 @@ for FourTimesTable in range (4,49,4):
   num=num+1
 
 
-
+#HLT 2: Going Loopy
+myname=input(("Please enter your name :"))
+total = 0
+while total <= 50:
+    num = int(input("Enter a number: "))
+    total += num
+    print("The total is",total,)
+  
