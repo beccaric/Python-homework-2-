9a9715ac4c18155ef4091a9405dc767c260d7c0d
becca <93043262+beccaric@users.noreply.github.com>
2022-10-09 12:20:44 +0000

    HLT 1 - 4 x tables

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 #Python Homework: Session 2 - 09 October 
 
 #HLT 1: Times Tables - For Loop
+
+num=1
+for FourTimesTable in range (4,49,4):
+  print(num, "x 4 = ", FourTimesTable)
+  num=num+1
+
+
+
